ec28afbcbe8667dd73f4a9e0e7450c89442f7985
Piero Madar <madarp@pobox.com>
2018-12-05 15:41:51 -0500

    cleanup test cases

--- tests/test_anagrams.py
+++ tests/test_anagrams.py
@@ -6,30 +6,36 @@ from anagrams import find_anagrams
 
 class TestAnagrams(unittest.TestCase):
     """
-        Bencharmking test case. We test actual functionality of find_anagrams
+        Benchmarking test case. We test actual functionality of find_anagrams
         with doctests, which is why this test case excludes those unit tests.
     """
+
     def setUp(self):
-        self.short = []
-        self.long = []
+        with open("words/short.txt") as f:
+            self.short = f.read().split()
 
-        with open("words/short.txt", "r") as handle:
-            self.short = handle.read().split()
-
-        with open("words/long.txt", "r") as handle:
-            self.long = handle.read().split()
+        with open("words/long.txt") as f:
+            self.long = f.read().split()
 
     def test_short(self):
-        """ Test that find_anagrams runs in 1/1000 of a second or faster. """
-        time = Timer(lambda: find_anagrams(self.short)).timeit(number=1)
-        self.assertTrue(round(time, 3) <= 0.005,
-                        "find_anagrams ran in {}, which exceeds the "
-                        "threshhold of 0.001 seconds".format(round(time, 3)))
+        """ Test find_anagrams with short word list. """
+        benchmark = 0.005  # seconds
+        my_time = Timer(lambda: find_anagrams(self.short)).timeit(number=1)
+        my_time = round(my_time, 3)
+        failure_text = (
+            'find_anagrams took {} seconds, which exceeds the '
+            'benchmark of {} seconds'.format(my_time, benchmark)
+            )
+        self.assertTrue(my_time <= benchmark, failure_text)
 
     @unittest.skip("Remove this line once short test passes")
     def test_long(self):
-        """ Test that find_anagrams runs in 1/10 of a second or faster. """
-        time = Timer(lambda: find_anagrams(self.long)).timeit(number=1)
-        self.assertTrue(round(time, 3) <= 0.10,
-                        "find_anagrams ran in {}, which exceeds the "
-                        "threshhold of 0.10 seconds".format(round(time, 3)))
+        """ Test find_anagrams with long word list. """
+        benchmark = 0.010  # seconds
+        my_time = Timer(lambda: find_anagrams(self.long)).timeit(number=1)
+        my_time = round(my_time, 3)
+        failure_text = (
+            'find_anagrams took {} seconds, which exceeds the '
+            'benchmark of {} seconds'.format(my_time, benchmark)
+            )
+        self.assertTrue(my_time <= benchmark, failure_text)
